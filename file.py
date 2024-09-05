11bb93aaca55f55a2378437cfca7922c959cf159
Zin_Ko <zinkolaymgy@gmail.com>
2024-09-05 13:34:29 +0700

    complete project

---
+++ file.py
@@ -0,0 +1,5 @@
+with open("/Users/User/Desktop/File collections/Zin Ko files/Draft.xlsx")as file:
+    contents = file.read()
+    print(contents)
+
+# \Users\User\Desktop\File collections\Zin Ko files
