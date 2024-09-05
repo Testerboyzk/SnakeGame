f3c3509bfa6b56aeb81cb3d0cd9ae14fb0329d2e
Zin_Ko <zinkolaymgy@gmail.com>
2024-09-05 13:38:28 +0700

    delete unwanted file

--- file.py
+++
@@ -1,5 +0,0 @@
-with open("/Users/User/Desktop/File collections/Zin Ko files/Draft.xlsx")as file:
-    contents = file.read()
-    print(contents)
-
-# \Users\User\Desktop\File collections\Zin Ko files
